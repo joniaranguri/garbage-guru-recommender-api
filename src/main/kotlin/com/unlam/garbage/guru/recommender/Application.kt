ac47aea62fce9f6bb426ac6c7f545549a28532ba
Jonathan Aranguri <arangurim@gmail.com>
2024-11-21 01:03:38 -0300

    feat: Moving Application to a valid package

--- src/main/kotlin/com/unlam/garbage/guru/recommender/Application.kt
+++ src/main/kotlin/com/unlam/garbage/guru/recommender/Application.kt
@@ -1,16 +1,14 @@
+package com.unlam.garbage.guru.recommender
+
 import io.ktor.http.*
 import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import model.material.Material
-import org.kie.api.KieServices
-import org.kie.api.runtime.KieContainer
-import org.kie.api.runtime.KieSession
 import org.slf4j.LoggerFactory
 import utils.KnowledgeSessionHelper
 
